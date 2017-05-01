d5d170cc15bc17e65874fe4fbea099c34a0a9af2
Alain Dechtjar <alain.dechtjar@haw-hamburg.de>
2017-05-01 17:53:53 +0200

    [ADDED] VR compatability; Pick up ball

---
+++ imp_beerpong_unity/Assets/Scripts/ControllerManagerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+ 
+public class ControllerManagerScript : MonoBehaviour {
+ 
+    public GameObject ball;
+	public GameObject pointer;
+
+	private bool pickedUp = false;
+ 
+    void Start () {
+    }
+ 
+    void Update () {
+ 
+        if (GvrController.ClickButton) {
+			BallInHand();
+			pickedUp = true;
+		}
+		if (!GvrController.ClickButton && pickedUp){
+            ThrowBall ();
+			pickedUp = false;
+		}
+ 
+    }
+ 
+    void ThrowBall () {
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+		rb.AddForce(ball.transform.forward * 500);
+    }
+
+	void BallInHand() {
+		Vector3 pos = pointer.transform.position;
+		Quaternion rot = pointer.transform.rotation;
+		ball.transform.SetPositionAndRotation (pos, rot);
+	}
+}
